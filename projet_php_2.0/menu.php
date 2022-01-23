eecfbcb4bed4787517d9889b5606c47967ea5d9e
chérubin <cherubinrodrigue.kaddas@ynov.com>
2022-01-23 23:22:46 +0100

    Hello

---
+++ projet_php_2.0/menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="style_1.css">
+        <title>Menus</title>
+    </head>
+    <body>
+        <div class="container">
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th><a href="index.php">Accueil</a></th>
+                        <th><a href="user_profil.php">Profil</a></th>
+                        <th><a href="redaction.php">Créer</a></th>
+                        <th><a href="logout.php">Deconnexion</a></th>
+                    </tr>
+                </thead>
+            </table>
+        </div>>
+        </div>
+    </body>
+</html>

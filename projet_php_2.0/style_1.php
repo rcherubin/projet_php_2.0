7c178b51d17261e473a72d39c1610d6f3a77c60f
chérubin <cherubinrodrigue.kaddas@ynov.com>
2022-01-23 23:23:33 +0100

    Hello

---
+++ projet_php_2.0/style_1.php
@@ -0,0 +1,80 @@
+/* .container {
+    border : 1.5px solid rgb(148, 135, 135);
+    height: 40px;
+} */
+
+
+.tab {
+    border-collapse: collapse;
+    min-width: 200px;
+    border : 1px solid black;  
+}
+
+.table {
+    border-collapse: collapse;
+    min-width: 850px;
+    /* width : auto; */
+    border : 1px solid rgb(37, 32, 32);
+    height : 40px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+thead tr{
+    background-color : rgb(17, 115, 172);
+    color : red;
+}
+
+/* tr th{
+    border : 1.5px solid black;
+} */
+
+a {
+    text-decoration: none;
+    color : rgb(230, 201, 201);
+}
+
+.cnt {
+    border : transparent;
+    width : 840px;
+    height : auto;
+    margin-left: 16.3%;
+    margin-top : 1px;
+}
+
+.articles {
+    border-bottom: 1px black;
+    width: 500px;
+    height: auto;
+}
+
+.box {
+    border-bottom: 1px solid rgb(117, 110, 110);
+    margin-right : 2%;
+}
+
+
+.profil h1 {
+    text-align: center;
+}
+
+.title-user {
+    width: auto;
+    background-color: rgb(95, 82, 82);
+    color: white;
+}
+
+.box-container {
+    width: 820px;
+    border: transparent;
+}
+
+#date {
+    color : rgb(95, 182, 216);
+    font-size : 10px;
+}
+
+#title-user {
+    text-decoration: underline;
+    font-size : 20px;
+}
